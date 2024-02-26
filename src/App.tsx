b55681d82ded55a5ef101658ade55da103df8f75
Umutcan-YEL <umutcanyel132@gmail.com>
2024-02-26 20:45:16 +0300

    stream.io change

---
+++ src/App.tsx
@@ -0,0 +1,96 @@
+import {
+  CallControls,
+  CallingState,
+  ParticipantView,
+  SpeakerLayout,
+  StreamCall,
+  StreamTheme,
+  StreamVideo,
+  StreamVideoClient,
+  StreamVideoParticipant,
+  useCallStateHooks,
+  User,
+} from "@stream-io/video-react-sdk";
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+
+const apiKey = "mmhfdzb5evj2"; // the API key can be found in the "Credentials" section
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiR3JlZWRvIiwiaXNzIjoiaHR0cHM6Ly9wcm9udG8uZ2V0c3RyZWFtLmlvIiwic3ViIjoidXNlci9HcmVlZG8iLCJpYXQiOjE3MDg5NjQ3OTMsImV4cCI6MTcwOTU2OTU5OH0.56IVWDhPY-WkMfMZvHa9c1UIjVTqLVN8rHY5aKd0SgQ"; // the token can be found in the "Credentials" section
+const userId = "Greedo"; // the user id can be found in the "Credentials" section
+const callId = "54ddcmmmYUGu"; // the call id can be found in the "Credentials" section
+
+// set up the user object
+const user: User = {
+  id: userId,
+  name: "Oliver",
+  image: "https://getstream.io/random_svg/?id=oliver&name=Oliver",
+};
+
+const client = new StreamVideoClient({ apiKey, user, token });
+const call = client.call("default", callId);
+call.join({ create: true });
+
+export const MyUILayout = () => {
+  const { useCallCallingState } = useCallStateHooks();
+  const callingState = useCallCallingState();
+  if (callingState !== CallingState.JOINED) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <StreamTheme style={{ position: "relative" }}>
+      <SpeakerLayout participantsBarPosition="bottom" />
+      <CallControls />
+    </StreamTheme>
+  );
+};
+
+export default function App() {
+  return (
+    <StreamVideo client={client}>
+      <StreamCall call={call}>
+        <MyUILayout />
+      </StreamCall>
+    </StreamVideo>
+  );
+}
+
+export const MyParticipantList = (props: {
+  participants: StreamVideoParticipant[];
+}) => {
+  const { participants } = props;
+  return (
+    <div style={{ display: "flex", flexDirection: "row", gap: "8px" }}>
+      {participants.map((participant) => (
+        <div style={{ width: "100%", aspectRatio: "3 / 2" }}>
+          <ParticipantView
+            muteAudio
+            participant={participant}
+            key={participant.sessionId}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export const MyFloatingLocalParticipant = (props: {
+  participant?: StreamVideoParticipant;
+}) => {
+  const { participant } = props;
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: "15px",
+        left: "15px",
+        width: "240px",
+        height: "135px",
+        boxShadow: "rgba(0, 0, 0, 0.1) 0px 0px 10px 3px",
+        borderRadius: "12px",
+      }}
+    >
+      {participant && <ParticipantView muteAudio participant={participant} />}
+    </div>
+  );
+};
---
+++ src/MyParticipantList.tsx
@@ -0,0 +1,14 @@
+import { ParticipantView, StreamVideoParticipant } from '@stream-io/video-react-sdk';
+
+// ... rest of the App.tsx code
+
+export const MyParticipantList = (props: { participants: StreamVideoParticipant[] }) => {
+  const { participants } = props;
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row', gap: '8px' }}>
+      {participants.map((participant) => (
+        <ParticipantView participant={participant} key={participant.sessionId} />
+      ))}
+    </div>
+  );
+};
